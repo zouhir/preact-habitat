abb4d97316d072d62c67ea217bbb81a1c0f09831
Zouhir <zouhir@zouhir.org>
2017-08-09 23:08:21 +1000

    jest tests 86% (stmts) - 100% (funcs) coverage

--- src/test/lib.test.js
+++ src/test/lib.test.js
@@ -1,62 +1,82 @@
 import { h } from 'preact';
-import { expect } from 'chai';
 import {
   collectPropsFromElement,
   widgetDOMHostElements,
   getExecutedScript,
   camelcasize,
   getHabitatSelectorFromClient
-} from '../src/lib';
+} from '../lib';
 
-import habitat from '../src';
+import habitat from '../index';
 
 describe('Helper utility: Camel Casing for props', () => {
   it('should not camcelCase names with no dashes', () => {
     const propOne = 'somepropname';
     // document must find current script tag
-    expect(camelcasize(propOne)).to.equal(propOne);
+    expect(camelcasize(propOne)).toBe(propOne);
   });
 
   it('should camcelCase prop names with dashes `-`', () => {
     const propOne = 'some-prop-name';
     // document must find current script tag
-    expect(camelcasize(propOne)).to.equal('somePropName');
+    expect(camelcasize(propOne)).toBe('somePropName');
   });
 })
 
 describe('Helper utility: Client DOM querying with widgetDOMHostElements', () => {
   it('should find host using data attribute', () => {
+    document.body.innerHTML = `
+      <div data-widget="my-widget"></div>
+      <div data-widget="my-widget"></div>
+      <div data-widget="my-widget"></div>
+    `
     const hostHabitats = widgetDOMHostElements("[data-widget='my-widget']", { clientSpecified: false, inline: false, clean: false });
     // document must find current script tag
-    expect(hostHabitats.length).to.equal(3);
+    expect(hostHabitats.length).toBe(3);
   });
 
   it('should find host using class name', () => {
+    document.body.innerHTML = `
+      <div class="classy-widget"></div>
+      <div class="classy-widget"></div>
+      <div class="classy-widget"></div>
+    `
     const hostHabitats = widgetDOMHostElements(".classy-widget", { clientSpecified: false, inline: false, clean: false });
     // document must find current script tag
-    expect(hostHabitats.length).to.equal(3);
+    expect(hostHabitats.length).toBe(3);
   });
 
   it('should find host using ID', () => {
+    document.body.innerHTML = `
+      <div id="idee-widget"></div>
+    `
     const hostHabitats = widgetDOMHostElements("#idee-widget", { clientSpecified: false, inline: false, clean: false });
     // document must find current script tag
-    expect(hostHabitats.length).to.equal(1);
+    expect(hostHabitats.length).toBe(1);
   });
 
   it('should get the currently getting executed script tag', () => {
-    expect(getExecutedScript(document)).to.not.be.undefined;
+    document.body.innerHTML = `
+      <script></script>
+    `
+    expect(getExecutedScript(document)).toBeDefined();
   });
 
   it('should get habitats selectors from client script itself', () => {
+    document.body.innerHTML = `
+      <script id="find-mount-here" data-mount-in=".my-widget"></script>
+    `
     let currentScript = document.getElementById('find-mount-here')
-
-    expect(getHabitatSelectorFromClient(currentScript)).to.equal('.my-widget');
+    expect(getHabitatSelectorFromClient(currentScript)).toBe('.my-widget');
   });
 });
 
 
 describe('Helper utility: collecting Client DOM props with collectPropsFromElement', () => {
   it('should pass props down from the client\'s div', () => {
+    document.body.innerHTML = `
+      <div id="sucess-props-check" data-props-name="zouhir" data-props-key="11001100"></div>
+    `
     const habitatDiv = document.getElementById('sucess-props-check');
     const expectedProps = {
       name: 'zouhir',
@@ -64,17 +84,20 @@ describe('Helper utility: collecting Client DOM props with collectPropsFromEleme
     };
     const propsObj = collectPropsFromElement(habitatDiv);
     // document must find current script tag
-    expect(propsObj).to.deep.equal(expectedProps);
+    expect(propsObj).toEqual(expectedProps);
   });
 
   it('should accept data-props- as well as data-prop attributes on the div', () => {
-    const habitatDiv = document.getElementById('sucess-props-check2');
+    document.body.innerHTML = `
+      <div id="sucess-props-check" data-prop-name="zouhir" data-prop-key="11001100"></div>
+    `
+    const habitatDiv = document.getElementById('sucess-props-check');
     const expectedProps = {
       name: 'zouhir',
       key: '11001100'
     };
     const propsObj = collectPropsFromElement(habitatDiv);
     // document must find current script tag
-    expect(propsObj).to.deep.equal(expectedProps);
+    expect(propsObj).toEqual(expectedProps);
   });
 })
