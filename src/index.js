08f2f2ab91838bc876d6793f25f1bf30533c7322
Zouhir <zouhir@zouhir.org>
2017-09-22 14:15:29 +1000

    v3.1.0 improves stability & fixes clearing dom bug

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import { widgetDOMHostElements, _render } from "./lib";
 const habitat = Widget => {
   // Widget represents the Preact component we need to mount
   let widget = Widget;
-  // in case DOM has fired multipled readystate events, redner only once
-  let hasRendered = false;
   // preact root render helper
   let root = null;
 
@@ -19,26 +17,21 @@ const habitat = Widget => {
     let elements = widgetDOMHostElements({
       selector,
       inline,
-      clean,
       clientSpecified
     });
-    if (!hasRendered && elements.length > 0) {
-      hasRendered = true;
-      return _render(widget, elements, root);
-    }
-    // document is not ready - subscurib to readystatechange event
-    document.addEventListener("DOMContentLoaded", e => {
-      if (!hasRendered && elements.length > 0) {
+    let loaded = () => {
+      if (elements.length > 0) {
         let elements = widgetDOMHostElements({
           selector,
           inline,
-          clean,
           clientSpecified
         });
-        hasRendered = true;
-        return _render(widget, elements, root);
+        return _render(widget, elements, root, clean);
       }
-    });
+    };
+    loaded();
+    document.addEventListener("DOMContentLoaded", loaded);
+    document.addEventListener("load", loaded);
   };
 
   return { render };
