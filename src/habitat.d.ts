a2251c26cf5bd4fd4877d1e451c3bf7a9d8db12f
Nathan Graule <solarliner@gmail.com>
2018-09-16 12:45:29 +0200

    Add documentation to configuration options

--- src/habitat.d.ts
+++ src/habitat.d.ts
@@ -1,10 +1,33 @@
 declare module "preact-habitat" {
   import { ComponentFactory } from "preact";
   interface IHabitatRenderConfig {
+    /**
+     * DOM Element selector used to retrieve the DOM elements you want to mount
+     * the widget in.
+     */
     selector: string;
+    /**
+     * Default props to be rendered throughout widgets, you can replace each
+     * value declaring props.
+     * @default {}
+     */
     defaultProps?: any;
+    /**
+     * Set to true if you want to use the parent DOM node as a host for your
+     * widget without specifing any selectors.
+     * @default true
+     */
     inline?: boolean;
+    /**
+     * clean will remove all the innerHTML from the HTMl element the widget will
+     * mount in.
+     * @default false
+     */
     clean?: boolean;
+    /**
+     * Whether the client will specify the selector. Overwrites selector option.
+     * @default false
+     */
     clientSpecified?: boolean;
   }
   interface IHabitat {
